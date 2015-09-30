12d66abf9d4e691cfc5c2b9136ecc2f085359c49
Brian Kelley <brian@virsix.com>
2015-09-30 11:23:11 -0700

    Almost got enemycontact anim done.... whooooooooooooooooooooooooooo

--- Assets/Scripts/enemycontact.cs
+++ Assets/Scripts/enemycontact.cs
@@ -1,29 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class enemycontact : MonoBehaviour 
+// Require these components when using this script
+// a reference to the capsule collider of the character
+
+
+public class enemycontact : MonoBehaviour
 {
 
     public float Health = 150f;
+   
     private float EnemyHealth;
     // Use this for initialization
-
-
-
-
     void Start()
     {
-        EnemyHealth = Health - 30.0f;
-
+        EnemyHealth = Health - 30f;
     }
-
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.GetComponent<Rigidbody>() == null) return;
-
-        //if we are hit by a ball
-        if (col.gameObject.tag == "enemy")
+          //if we are hit by a ball
+        if (col.gameObject.tag == "ball")
         {
             GetComponent<AudioSource>().Play();
             Destroy(gameObject);
@@ -33,22 +31,20 @@ public class enemycontact : MonoBehaviour
             //calculate the damage via the hit object velocity
             float damage = col.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 10;
             Health -= damage;
+
             //don't play sound for small damage
             if (damage >= 10)
+            {
                 GetComponent<AudioSource>().Play();
-            /*
-            //Change animation hear when worked out
-            if (Health < ChangeSpriteHealth)
-            {//change the shown sprite
-                GetComponent<SpriteRenderer>().sprite = SpriteShownWhenHurt;
             }
-            */
-           
 
+            if (Health < EnemyHealth)
+            {//change the shown animation
+                Debug.Log("dkjlhfalkdsajlkfjkaldfjlkdasjfkldsjflkdjfoiwekfjkdlnfkjefijfkdjfkeajifjdskjfiejflkdjflka");
+                GetComponent<Animation>().Play("Shotgun");
                 
+            }
             if (Health <= 0) Destroy(this.gameObject);
         }
     }
-
-
 }
