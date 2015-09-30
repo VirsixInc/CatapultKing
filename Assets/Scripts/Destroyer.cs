51932b9c177b3e0bfd95282bc2cc2cad0343f679
Brian Kelley <brian@virsix.com>
2015-09-30 07:54:29 -0700

    Added Destroyer script... As safe guard to destroy balls, blocks, enemies if off play field.

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        //destroyers are located in the borders of the screen
+        //if something collides with them, the'll destroy it
+        string tag = col.gameObject.tag;
+        if (tag == "block" || tag == "enemy" || tag == "ball")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
