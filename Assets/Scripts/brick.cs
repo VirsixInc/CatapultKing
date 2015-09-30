2a03faa342fe0a33768c9abf7e7e2c84d73297e9
Brian Kelley <brian@virsix.com>
2015-09-30 07:41:14 -0700

    Add brick health script

---
+++ Assets/Scripts/brick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brick : MonoBehaviour
+{
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody>() == null) return;
+
+        float damage = col.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 10;
+        //don't play audio for small damages
+        if (damage >= 10)
+            GetComponent<AudioSource>().Play();
+        //decrease health according to magnitude of the object that hit us
+        Health -= damage;
+        //if health is 0, destroy the block
+        if (Health <= 0) Destroy(this.gameObject);
+        Debug.Log(Health);
+    }
+    
+    public float Health = 70f;
+    
+
+}
