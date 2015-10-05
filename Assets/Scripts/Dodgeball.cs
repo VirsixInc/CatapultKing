1d73bb8d7f8e3f40622ee735e36e8b5cfb36f89c
Brian Kelley <brian@virsix.com>
2015-10-05 10:40:40 -0700

    Made particle helper script and created some sweet explosions

--- Assets/Scripts/Dodgeball.cs
+++ Assets/Scripts/Dodgeball.cs
@@ -6,12 +6,16 @@ public class Dodgeball : MonoBehaviour {
     public float bulletlifetime = 3.0f;
     public GameObject hitParticle;
     public float partlifetime = 0.1f;
-    public ParticleEmitter hitpart;
+    public ParticleSystem hitpart;
+
+    //public ParticleEmitter hitpart;
 
     // Update is called once per frame
-    void Update () {
-	
-	}
+    void Update () {}
+    void Start()
+    {
+        ParticleSystem hitpart = GetComponent<ParticleSystem>();
+    }
     /*
     void FixedUpdate()
     {
@@ -27,21 +31,32 @@ public class Dodgeball : MonoBehaviour {
 
 
 
-    void OnCollisionEnter(Collision coll){
-    if(coll.collider.gameObject.tag == "block")
+    void OnCollisionEnter(Collision coll) {
+        if (coll.collider.gameObject.tag == "block")
         {
             coll.collider.gameObject.GetComponent<blockScript>().broken = true;
             GameObject[] shatters = TurboSlice.instance.shatter(coll.collider.gameObject, 2);
-            
-            foreach (GameObject shattered in shatters)
-                {
-                    shattered.GetComponent<Rigidbody>().velocity = shattered.GetComponent<Rigidbody>().velocity/2;
 
-                }
+            foreach (GameObject shattered in shatters)
+            {
+                shattered.GetComponent<Rigidbody>().velocity = shattered.GetComponent<Rigidbody>().velocity / 2;
+
+            }
+           
+
         }
-        GameObject.Instantiate(hitParticle, transform.position, Quaternion.identity);
+        SpecialEffectsHelper.Instance.Explosion(transform.position);
+
+        //GameObject.Instantiate(hitParticle, transform.position, Quaternion.identity);
         Destroy(GameObject.FindGameObjectWithTag("ball"), bulletlifetime);
         //hitpart.Emit();
+       /* if (hitpart)
+        {
+            if (!hitpart.IsAlive())
+                Destroy(gameObject);
+        }
+        */
+
         Destroy(GameObject.FindGameObjectWithTag("particle"), partlifetime);
         
     }
